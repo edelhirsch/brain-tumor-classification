8fbeba7d509705191488ff456fa85a9db3add201
Peter Hartmann <peter@edelhirsch.io>
2020-10-30 17:00:27 +0100

    build model

--- main.py
+++ main.py
@@ -4,46 +4,90 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import glob
-# from keras import layers
-# from keras import models
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
 from os import path
 import os
 import shutil
 
 base_dir = "/home/peter/dev/brain-tumors"
+original_data_dir = base_dir + "/original-data"
+original_training_dir = original_data_dir + "/Training"
+original_testing_dir = original_data_dir + "/Testing"
+
 data_dir = base_dir + "/data"  # use os.path.join
 training_dir = data_dir + "/Training"
 validation_dir = data_dir + "/Validation"
+testing_dir = data_dir + "/Testing"
+
 categories = ['glioma_tumor', 'meningioma_tumor', 'no_tumor', 'pituitary_tumor']
 
 
-def prepare_data():
+def build_model():
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                            input_shape=(128, 128, 1)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(4, activation='softmax'))
 
-    if not path.exists(data_dir):
-        print(f'{data_dir} does not exist, preparing data...', end=' ')
-        original_data_dir = base_dir + "/original-data"
-        shutil.copytree(original_data_dir, data_dir)
-        os.mkdir(validation_dir)
+    # model.summary()
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizers.RMSprop(),
+                  metrics=['acc'])
 
-        # Move 100 files of each dir to validation data
-        for category in categories:
-            current_dir = training_dir + "/" + category
-            validation_dir_cat = validation_dir + "/" + category
-            os.mkdir(validation_dir_cat)
+    train_datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.15)
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-            for f in glob.glob(current_dir + "/*.jpg")[:100]:
-                shutil.move(f, validation_dir_cat)
+    if not path.exists(training_dir):
+        os.mkdir(data_dir)
+        os.mkdir(training_dir)
 
-        print('done')
+    train_generator = train_datagen.flow_from_directory(
+        original_training_dir,
+        target_size=(128, 128),
+        color_mode='grayscale',
+        class_mode='categorical',
+        batch_size=32,
+        save_to_dir=training_dir,
+        save_prefix='',
+        save_format='jpeg',
+        subset='training'
+    )
 
+    validation_generator = train_datagen.flow_from_directory(
+        original_training_dir,
+        target_size=(128, 128),
+        color_mode='grayscale',
+        class_mode='categorical',
+        batch_size=32,
+        subset='validation'
+    )
 
-def load_images():
-    testing_dir = data_dir + "/Testing"
-    print(f'here load images')
+    history = model.fit(
+        x=train_generator,
+        epochs=30,
+        verbose=2,
+    )
 
+    # history = model.fit_generator(
+    #     train_generator,
+    #     steps_per_epoch=100,
+    #     Batch_size=2,
+    #     epochs=30,
+    #     validation_data=validation_generator,
+    #     validation_steps=50)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    prepare_data()
-    load_images()
+    build_model()
 
