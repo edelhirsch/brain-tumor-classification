5e581bf277ec76442cb968ce617d2adf40c43b6d
Peter Hartmann <peter@edelhirsch.io>
2020-11-02 17:18:00 +0100

    use bigger image size

--- main.py
+++ main.py
@@ -26,9 +26,12 @@ categories = ['glioma_tumor', 'meningioma_tumor', 'no_tumor', 'pituitary_tumor']
 
 
 def build_model():
+    ### (1) try increasing image size even more (256 is too high)
+    img_size = 192
+
     model = models.Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu',
-                            input_shape=(128, 128, 1)))
+                            input_shape=(img_size, img_size, 1)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
@@ -37,6 +40,8 @@ def build_model():
     model.add(layers.Conv2D(128, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
+    ### (1) add dropout layer
+    ### (1) L2 regularisation
     model.add(layers.Dense(512, activation='relu'))
     model.add(layers.Dense(4, activation='softmax'))
 
@@ -45,6 +50,8 @@ def build_model():
                   optimizer=optimizers.RMSprop(),
                   metrics=['acc'])
 
+    ### (2) try changing the validation split
+    ### (2) play with options of ImageDataGenerator
     train_datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.15)
     test_datagen = ImageDataGenerator(rescale=1. / 255)
 
@@ -52,9 +59,10 @@ def build_model():
         os.mkdir(data_dir)
         os.mkdir(training_dir)
 
+    ### (3) cross validation?
     train_generator = train_datagen.flow_from_directory(
         original_training_dir,
-        target_size=(128, 128),
+        target_size=(img_size, img_size),
         color_mode='grayscale',
         class_mode='categorical',
         batch_size=32,
@@ -66,7 +74,7 @@ def build_model():
 
     validation_generator = train_datagen.flow_from_directory(
         original_training_dir,
-        target_size=(128, 128),
+        target_size=(img_size, img_size),
         color_mode='grayscale',
         class_mode='categorical',
         batch_size=32,
@@ -80,13 +88,8 @@ def build_model():
         validation_data=validation_generator,
     )
 
-    # history = model.fit_generator(
-    #     train_generator,
-    #     steps_per_epoch=100,
-    #     Batch_size=2,
-    #     epochs=30,
-    #     validation_data=validation_generator,
-    #     validation_steps=50)
+    model.save('brain-tumor-classification.h5')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
