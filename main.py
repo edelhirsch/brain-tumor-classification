2dbd94a9a2600d0f748859e11a8e6f78d4448c59
Peter Hartmann <peter@edelhirsch.io>
2020-11-10 13:12:09 +0100

    try with transfer learning

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import glob
+import keras
 from keras import layers
 from keras import models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import numpy as np
 from os import path
 import os
 import shutil
+import tensorflow as tf
 
 base_dir = "/home/peter/dev/brain-tumors"
 original_data_dir = base_dir + "/original-data"
@@ -23,115 +27,107 @@ categories = ['glioma_tumor', 'meningioma_tumor', 'no_tumor', 'pituitary_tumor']
 def build_model():
     ### (1) try increasing image size even more (256 is too high)
     ### set this to 128 to make it go faster:
-    img_size = 150
-    epochs = 50
-    batch_size = 40
+    size = (150, 150)
+    input_shape = size + (3,)
+    epochs = 20
+    batch_size = 32
 
-    model = models.Sequential()
-    #
-    model.add(layers.Conv2D(filters = 64, kernel_size = (5,5),padding = 'Same', 
-                     activation ='relu', input_shape = (img_size,img_size,1)))
-    model.add(layers.MaxPool2D(pool_size=(2,2)))
-    model.add(layers.Dropout(0.25))
-    #
-    model.add(layers.Conv2D(filters = 128, kernel_size = (3,3),padding = 'Same', 
-                     activation ='relu'))
-    model.add(layers.MaxPool2D(pool_size=(2,2), strides=(2,2)))
-    model.add(layers.Dropout(0.25))
-    #
-    model.add(layers.Conv2D(filters = 128, kernel_size = (3,3),padding = 'Same', 
-                     activation ='relu'))
-    model.add(layers.MaxPool2D(pool_size=(2,2), strides=(2,2)))
-    model.add(layers.Dropout(0.3))
-    #
-    model.add(layers.Conv2D(filters = 128, kernel_size = (2,2),padding = 'Same', 
-                     activation ='relu'))
-    model.add(layers.MaxPool2D(pool_size=(2,2), strides=(2,2)))
-    model.add(layers.Dropout(0.3))
-    
-    #
-    model.add(layers.Conv2D(filters = 256, kernel_size = (2,2),padding = 'Same', 
-                     activation ='relu'))
-    model.add(layers.MaxPool2D(pool_size=(2,2), strides=(2,2)))
-    model.add(layers.Dropout(0.3))
-    
-    # 
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1024, activation = "relu"))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(4, activation = "softmax"))
-    #optimizer = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
-    optimizer = optimizers.Adam()
-    model.compile(optimizer = optimizer, loss = "categorical_crossentropy", metrics=["accuracy"])
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(original_training_dir,
+                                                                   label_mode='categorical',
+                                                                   color_mode='rgb',  # or rgb?
+                                                                   image_size=size,
+                                                                   batch_size=batch_size,
+                                                                   validation_split=0.2,
+                                                                   subset='training',
+                                                                   seed=1337,
+                                                                   )
+    print(f'classes found: {train_ds.class_names}')
+    print(f'train ds: {train_ds}')
+    for image_batch, labels_batch in train_ds:
+        print(image_batch.shape)
+        print(labels_batch.shape)
+        break
 
+    validation_ds = tf.keras.preprocessing.image_dataset_from_directory(original_training_dir,
+                                                                        label_mode='categorical',
+                                                                        color_mode='rgb',
+                                                                        image_size=size,
+                                                                        batch_size=batch_size,
+                                                                        validation_split=0.2,
+                                                                        subset='validation',
+                                                                        seed=1337,
+                                                                        )
 
-    if not path.exists(data_dir):
-        os.mkdir(data_dir)
+    test_ds = tf.keras.preprocessing.image_dataset_from_directory(original_testing_dir,
+                                                                  label_mode='categorical',
+                                                                  color_mode='rgb',
+                                                                  image_size=size,
+                                                                  batch_size=batch_size,
+                                                                  )
 
-    if path.exists(training_dir):
-        shutil.rmtree(training_dir)
+    # train_ds = train_ds.cache().batch(batch_size).prefetch(buffer_size=10)
+    # validation_ds = validation_ds.cache().batch(batch_size).prefetch(buffer_size=10)
+    # test_ds = test_ds.cache().batch(batch_size).prefetch(buffer_size=10)
 
-    os.mkdir(training_dir)
-
-    ### (2) try changing the validation split
-    ### (2) play with options of ImageDataGenerator
-    ### (1) add augmentation featuers of train_datagen explicitly
-
-    train_datagen = ImageDataGenerator(
-        rescale=1. / 255,
-        validation_split=0.2,
-        zoom_range=0.0,
-        # shear_range=0.0,
-        width_shift_range=0.0,
-        height_shift_range=0.0,
-        horizontal_flip=True,  
-
-
-        featurewise_center=False,  
-        samplewise_center=False, 
-        featurewise_std_normalization=False,  
-        samplewise_std_normalization=False,  
-        zca_whitening=False,  
-        rotation_range=0.0,
-        vertical_flip=False
-    )
-    test_datagen = ImageDataGenerator(rescale=1. / 255)
-
-    ### (3) cross validation?
-    train_generator = train_datagen.flow_from_directory(
-        original_training_dir,
-        target_size=(img_size, img_size),
-        color_mode='grayscale',
-        class_mode='categorical',
-        batch_size=batch_size,
-        #save_to_dir=training_dir,
-        #save_prefix='',
-        #save_format='jpeg',
-        subset='training'
+    data_augmentation = keras.Sequential(
+        [
+            layers.experimental.preprocessing.RandomFlip("horizontal"),
+        ]
     )
 
-    validation_generator = train_datagen.flow_from_directory(
-        original_training_dir,
-        target_size=(img_size, img_size),
-        color_mode='grayscale',
-        class_mode='categorical',
-        batch_size=batch_size,
-        subset='validation'
+    # for images, labels in train_ds.take(1):
+    #     plt.figure(figsize=(10, 10))
+    #     first_image = images[0]
+    #     for i in range(9):
+    #         ax = plt.subplot(3, 3, i + 1)
+    #         augmented_image = data_augmentation(
+    #             tf.expand_dims(first_image, 0), training=True
+    #         )
+    #         plt.imshow(augmented_image[0].numpy().astype("int32"))
+    #         plt.title(int(labels[i]))
+    #         plt.axis("off")
+
+    base_model = keras.applications.Xception(
+        weights="imagenet",  # Load weights pre-trained on ImageNet.
+        input_shape=input_shape,
+        include_top=False,
+    )  # Do not include the ImageNet classifier at the top.
+
+    # Freeze the base_model
+    base_model.trainable = False
+
+    # Create new model on top
+    inputs = keras.Input(shape=input_shape)
+    x = data_augmentation(inputs)  # Apply random data augmentation
+
+    # Pre-trained Xception weights requires that input be normalized
+    # from (0, 255) to a range (-1., +1.), the normalization layer
+    # does the following, outputs = (inputs - mean) / sqrt(var)
+    norm_layer = keras.layers.experimental.preprocessing.Normalization()
+    mean = np.array([127.5] * 3)
+    var = mean ** 2
+    # Scale inputs to [-1, +1]
+    x = norm_layer(x)
+    norm_layer.set_weights([mean, var])
+
+    # The base model contains batchnorm layers. We want to keep them in inference mode
+    # when we unfreeze the base model for fine-tuning, so we make sure that the
+    # base_model is running in inference mode here.
+    x = base_model(x, training=False)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    x = keras.layers.Dropout(0.2)(x)  # Regularize with dropout
+    outputs = keras.layers.Dense(4)(x)
+    model = keras.Model(inputs, outputs)
+
+    model.summary()
+
+    model.compile(
+        optimizer=keras.optimizers.Adam(),
+        loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+        metrics=[keras.metrics.CategoricalAccuracy()],
     )
 
-    history = model.fit(
-        x=train_generator,
-        epochs=epochs,
-        verbose=2,
-        validation_data=validation_generator,
-    )
-
-    #history = model.fit_generator(
-        #train_generator,
-        #epochs=epochs,
-        #steps_per_epoch=57,
-        #validation_data=validation_generator,
-        #validation_steps=14)
+    model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
 
     model.save('brain-tumor-classification.h5')
 
